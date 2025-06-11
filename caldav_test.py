a7a78eb8cb0e2d880a8e28a408e4b59cac6a36ad
JochiSt <jochen.steinmann@web.de>
2025-06-11 19:59:59 +0200

    wrapped functions around

--- caldav_test.py
+++ caldav_test.py
@@ -15,26 +15,27 @@ url  = "https://"+ NEXTCLOUD_HOSTNAME +"/remote.php/dav/calendars/"
 url += CALDAV_USER_NAME + "/"
 url += CALDAV_CALENDAR_NAME + "/"
 
-today  = datetime.combine(datetime.today(), time(0,0))
+def calDAVconnect():
+    # connect to nextcloud
+    client = caldav.DAVClient(url,
+                              username=credentials.caldav_login["username"],
+                              password=credentials.caldav_login["password"],
+                              ssl_verify_cert=False)
+    principal = client.principal()
+    calendars = principal.calendars()
 
-# connect to nextcloud
-client = caldav.DAVClient(url,
-                          username=credentials.caldav_login["username"],
-                          password=credentials.caldav_login["password"],
-                          ssl_verify_cert=False)
-principal = client.principal()
-calendars = principal.calendars()
+    # search for the matching calendar
+    calendar = None
+    for cal in calendars:
+        if CALDAV_CALENDAR_NAME.upper() == str(cal).upper():
+            print("Calendar "+ CALDAV_CALENDAR_NAME +" found")
+            calendar = cal
+    return calendar
 
-# search for the matching calendar
-calendar = None
-for cal in calendars:
-    if CALDAV_CALENDAR_NAME.upper() == str(cal).upper():
-        print("Calendar "+ CALDAV_CALENDAR_NAME +" found")
-        calendar = cal
-
-if calendar:
-    may_event = calendar.save_event(
-        dtstart = date(2025, 6, 17),
-        dtend = date(2025, 6, 17),
-        summary = "Do the needful",
-    )
+def createWholeDayEvent(calendar, datum, summary):
+    if calendar:
+        event = calendar.save_event(
+            dtstart = datum,
+            dtend = datum,
+            summary = summary,
+        )
