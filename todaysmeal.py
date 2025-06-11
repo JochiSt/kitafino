5ea6705665d0c97e1724a45b2c1dabc4ca9f7679
JochiSt <jochen.steinmann@web.de>
2025-06-11 11:54:47 +0200

    use credentials and printout whole menu

--- todaysmeal.py
+++ todaysmeal.py
@@ -3,13 +3,13 @@ from bs4 import BeautifulSoup
 import re
 from datetime import datetime, timedelta
 
-
+import credentials 
 
 s = requests.Session()
 
 #login to kitafino
 s.get('https://www.kitafino.de')
-r_login  =s.post('https://www.kitafino.de/sys_k2/index.php?action=do_login', data={'passwort': 'YOUR_PASSWORD', 'benutzername': 'YOUR_USERNAME' })
+r_login  =s.post('https://www.kitafino.de/sys_k2/index.php?action=do_login', data={'passwort': credentials.login['password'], 'benutzername': credentials.login['username'] })
 
 
 #calculate epoch of monday this week 11:00
@@ -18,17 +18,13 @@ monday = now - timedelta(days = now.weekday())
 monday=monday.replace(hour=11,minute=0,second=0);
 monday= int((monday - datetime(1970, 1, 1)).total_seconds())
 
-
 params = {'kw_ts': monday}
 kitafino_raw = s.get('https://www.kitafino.de/sys_k2/index.php?action=bestellen', params=params)
 kitafino_close = s.get('https://www.kitafino.de/sys_k2/index.php?action=log_out')
 s.close()
 
-
-
 #use BeautifulSoup to extract needed Parts
 soup = BeautifulSoup(kitafino_raw.text, "html.parser")
-
 
 #search all <a name=menuexxxxxxxx> -> each on is start of one entry
 foo = soup.body.findAll('div',  {"class" : re.compile('order_table*')})
@@ -59,7 +55,7 @@ for bar in foo:
     listOfWords = order.split(" ", 1)
     if len(listOfWords) > 0: 
         order = listOfWords[1]
-    #print(order)
+    print(item_date, order)
     
     today=datetime.now()
     if item_date.year==today.year:
@@ -69,5 +65,3 @@ for bar in foo:
                 returnvalue=order
                 #print(mystring)
     
-print(returnvalue)
-
