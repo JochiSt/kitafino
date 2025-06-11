0c5933c52c21501f09b3596529399ef5c293917e
JochiSt <jochen.steinmann@web.de>
2025-06-11 14:24:12 +0200

    updated example credentials with some more variables, no-one needs to know

--- credentials_example.py
+++ credentials_example.py
@@ -1,5 +1,15 @@
 
-login = {
+kitafino_login = {
     'username' : 'YOUR_USERNAME',
     'password' : 'YOUR_PASSWORD',
 }
+
+
+caldav_login = {
+    'username' : 'YOUR_USERNAME',
+    'password' : 'YOUR_PASSWORD',
+}
+
+caldav_hostname = 'YOUR_HOSTNAME'
+caldav_calendar_name = 'YOUR_CALENDARNAME'
+caldav_user_name = 'YOUR_USERNAME'
